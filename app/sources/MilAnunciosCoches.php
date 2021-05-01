6f932c565c9e1d261e66546f12d7d9883133b008
Santi Márquez <santymaro@gmail.com>
2021-05-02 00:13:38 +0200

    Ternary operator corrected
    Use of $key in foreach instead of a created counter

--- app/sources/MilAnunciosCoches.php
+++ app/sources/MilAnunciosCoches.php
@@ -34,36 +34,32 @@ class MilAnunciosCoches
 
         //Create the response (array with Advertisement objects)
         $ads_array = array();
-        $add_counter = 0;
-        foreach ($array_response["adListPagination"]["adList"]["ads"] as $ad) {
-            $add_counter++;
-            $ads_array[$add_counter] = new Advertisement();
-            $ads_array[$add_counter]->source_id = $this->source_id;
-            $ads_array[$add_counter]->reference = $ad["id"];
-            $ads_array[$add_counter]->price = str_replace('.', '', $ad["price"]["value"]);
-            $ads_array[$add_counter]->url = $ad["url"];
-            if (isset($ad["images"][0]["src"])) {
-                $ads_array[$add_counter]->pic_url = $ad["images"][0]["src"];
-            }
+        foreach ($array_response["adListPagination"]["adList"]["ads"] as $key => $ad) {
+            $ads_array[$key] = new Advertisement();
+            $ads_array[$key]->source_id = $this->source_id;
+            $ads_array[$key]->reference = $ad["id"];
+            $ads_array[$key]->price = str_replace('.', '', $ad["price"]["value"]);
+            $ads_array[$key]->url = $ad["url"];
+            $ads_array[$key]->pic_url = (isset($ad["images"][0]["src"])) ? $ad["images"][0]["src"] : NULL;
 
-            $ads_array[$add_counter]->phone_number = $ad["firstPhoneNumber"];
+            $ads_array[$key]->phone_number = $ad["firstPhoneNumber"];
 
             foreach ($ad["tags"] as $tag) {
                 if ($tag["type"] === "kms") {
-                    $ads_array[$add_counter]->kms = str_replace(".", "", str_replace(" kms", "", $tag["text"]));
+                    $ads_array[$key]->kms = str_replace(".", "", str_replace(" kms", "", $tag["text"]));
                 }
                 if ($tag["type"] === "cambio") {
                     if ($tag["text"] === "Manual") {
-                        $ads_array[$add_counter]->gear = "M";
+                        $ads_array[$key]->gear = "M";
                     } else {
-                        $ads_array[$add_counter]->gear = "A";
+                        $ads_array[$key]->gear = "A";
                     }
                 }
                 if ($tag["type"] === "CV") {
-                    $ads_array[$add_counter]->power = str_replace(" CV", "", $tag["text"]);
+                    $ads_array[$key]->power = str_replace(" CV", "", $tag["text"]);
                 }
                 if ($tag["type"] === "au00F1o") {
-                    $ads_array[$add_counter]->year = $tag["text"];
+                    $ads_array[$key]->year = $tag["text"];
                 }
             }
         }
--- app/sources/Wallapop.php
+++ app/sources/Wallapop.php
@@ -18,21 +18,18 @@ Class Wallapop
 
         //Create the response (array with Advertisement objects)
         $ads_array = array();
-        $add_counter = 0;
-        foreach ($array_response["search_objects"] as $ad) {
-            $add_counter++;
-            $ads_array[$add_counter] = new Advertisement();
-            $ads_array[$add_counter]->source_id = $this->source_id;
-            $ads_array[$add_counter]->reference = $ad["content"]["id"];
-            $ads_array[$add_counter]->price = $ad["content"]["price"];
-            $ads_array[$add_counter]->url = 'https://es.wallapop.com/item/' . $ad["content"]["web_slug"];
-            $ads_array[$add_counter]->pic_url = $ad["content"]["images"][0]["original"];
+        foreach ($array_response["search_objects"] as $key =>$ad) {
+            $ads_array[$key] = new Advertisement();
+            $ads_array[$key]->source_id = $this->source_id;
+            $ads_array[$key]->reference = $ad["content"]["id"];
+            $ads_array[$key]->price = $ad["content"]["price"];
+            $ads_array[$key]->url = 'https://es.wallapop.com/item/' . $ad["content"]["web_slug"];
+            $ads_array[$key]->pic_url = $ad["content"]["images"][0]["original"];
 
-            $ads_array[$add_counter]->phone_number = NULL;
-            (isset($ad["content"]["km"])) ? $ads_array[$add_counter]->kms = $ad["content"]["km"] : $ads_array[$add_counter]->kms = NULL;
-            ($ad["content"]["gearbox"] == "manual") ? $ads_array[$add_counter]->gear = "M" : $ads_array[$add_counter]->gear = "A";
-            (isset($ad["content"]["horsepower"])) ? $ads_array[$add_counter]->power = $ad["content"]["horsepower"] : $ads_array[$add_counter]->power = NULL;
-            $ads_array[$add_counter]->year = $ad["content"]["year"];
+            $ads_array[$key]->kms = (isset($ad["content"]["km"])) ?  $ad["content"]["km"] : NULL;
+            $ads_array[$key]->gear = ($ad["content"]["gearbox"] == "manual") ? "M" : "A";
+            $ads_array[$key]->power = (isset($ad["content"]["horsepower"])) ? $ad["content"]["horsepower"] : NULL;
+            $ads_array[$key]->year = $ad["content"]["year"];
         }
 
         return $ads_array;
